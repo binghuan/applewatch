136e4a4da77c5816d10b2d0d2b82c704cbbb1d3c
BH_Lin <binghuanlin@gmail.com>
2014-12-08 23:53:05 +0800

    first check-in

---
+++ js/applewatch.js
@@ -0,0 +1,30 @@
+$$(document).ready(function(){
+	console.log('Document ready');
+	var $toucharea = $$('#toucharea');
+
+	$toucharea.on('swipeUp', function(e){
+   		console.log(new Date(),'swipeUp');
+   		$("#view_homescreen").hide();
+   		//$("#view_homescreen").fadeOut();
+   		$("#view_glaces").show();
+   		$('.carousel').carousel('pause')
+	});
+
+	$toucharea.on('swipeDown', function(e){
+   		console.log(new Date(),'swipeDown');
+   		$("#view_homescreen").show();
+   		$("#view_glaces").hide();
+	});
+
+	$toucharea.on('swipeLeft', function(e){
+   		console.log(new Date(),'swipeLeft');
+   		$('.carousel').carousel('next')
+   		$('.carousel').carousel('pause')
+	});
+
+	$toucharea.on('swipeRight', function(e){
+   		console.log(new Date(),'swipeRight');
+   		$('.carousel').carousel('prev')
+   		$('.carousel').carousel('pause')
+	});
+});
